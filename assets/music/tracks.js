d7c5ebfb656603bf62a52f391069b02db0374b49
WhoisCate <xuantungofficial@gmail.com>
2026-07-17 20:42:00 +0700

    Update tracks.js

--- assets/music/tracks.js
+++ assets/music/tracks.js
@@ -3,7 +3,7 @@ window.CATE_TRACKS = [
     title: "Live MIXSET #1",
     artist: "WhoisCatee",
     duration: "2:39:16",
-    year: "2026",
+    year: "2025",
     type: "Original",
     src: "https://github.com/CateDevLo/dj/releases/download/music/Live.MIXSET.1.mp3",
   },
