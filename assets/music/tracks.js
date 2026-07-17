531d8af2e40c37ecb5db8f572ca1c04392375b56
WhoisCate <xuantungofficial@gmail.com>
2026-07-17 20:53:01 +0700

    dj

--- assets/music/tracks.js
+++ assets/music/tracks.js
@@ -5,6 +5,14 @@ window.CATE_TRACKS = [
     duration: "2:39:16",
     year: "2025",
     type: "Original",
-    src: "https://ia601608.us.archive.org/28/items/live-mixset-1/Live%20MIXSET%20%231.mp3",
+    src: "https://github.com/CateDevLo/dj/releases/download/music/Live.MIXSET.1.mp3",
+  },
+  {
+    title: "Cate Trance Set",
+    artist: "WhoisCatee",
+    duration: "58:30",
+    year: "2026",
+    type: "Trance",
+    src: "assets/music/MixSet2.mp3",
   },
 ];
